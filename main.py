f166974da2425b9ca8933fc8f3bdaa1e2dfb2ad0
Leandro Soares Torres <Leandro.Torres@pio.com.br>
2022-12-26 10:28:53 -0300

    Atualizaões no código

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-def partida():
+def computador_escolhe_jogada(n, m):  #FUNÇÃO QUE EXECUTA AS JOGADAS DO COMPUTADOR
+    computador_retira = 1
+
+        # N restante == multiplo de (m+1) 
+    while computador_retira != m:
+        if (n - computador_retira) % (m + 1) == 0:
+            return computador_retira
+        else:
+            computador_retira += 1
+    return computador_retira
+
+
+def jogador_escolhe_jogada(n, m):
+    while True:
+        jogador_remove_peca = int(input('Quantas peças você vai retirar?'))
+        if jogador_remove_peca > m or jogador_remove_peca < 1:
+
+            print("jogada inválida")
+        else:
+            break
+    return jogador_remove_peca
+
+
+def campeonato():
+    rodada = 1
+    while rodada <= 3:
+        print(f'Rodada: {rodada}')
+        rodada +=1
+    
+
+
+
+def partida():  
     while True:
         try:        
             n = int(input('Digite a quantidade de peças'))
@@ -11,9 +43,10 @@ def partida():
             continue
     escolhe_primeiro_jogador(n, m)
 
-def escolhe_primeiro_jogador(n, m):
-    if (n % (m + 1)) == 0:
+def escolhe_primeiro_jogador(n, m): 
+    if n % (m + 1) == 0:
         print('Jogador Inicia')
+        computador_escolhe_jogada(n,m)
     else:
         print('Computador inicia')
 
